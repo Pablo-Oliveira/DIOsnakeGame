d68171abde029f251a9986b4a6fa285e7a44ea1e
Pablo Oliveira <pablue@gmail.com>
2021-09-02 21:31:37 -0400

    criar movimento snake

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
+let direction = "rigth";
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
@@ -19,5 +19,27 @@ function criarCobrinha() {
     }
 };
 
-criarBG();
-criarCobrinha();
+
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "rigth") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
